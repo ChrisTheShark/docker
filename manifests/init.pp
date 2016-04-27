d73ddc80e08c346ba4e54c533956bcef1021c54f
ChrisTheShark <cdyer1980@gmail.com>
2016-04-27 18:00:49 +0000

    Removing unneeded comments.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,16 +2,6 @@
 #
 # Module to install an up-to-date version of Docker from package.
 #
-# === Parameters
-# [*dockerrepo*]
-#	The file name for the various package systems. Defaults 
-#       to docker.repo.
-#
-# [*package*]
-#	The package name to install. Defaults to docker-engine.
-#
-# [*service*]
-#	The service name to start. Defaults to docker.
 class docker {
   include docker::config
   include docker::repository
